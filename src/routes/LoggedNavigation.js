50f2fe94462fc1ffac59f6bbafa8ff3502deec8e
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-04 14:26:42 -0300

    Add routes for users Logged

--- src/routes/LoggedNavigation.js
+++ src/routes/LoggedNavigation.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Button } from "semantic-ui-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Artists, Artist, Albums, Album, Profile } from "../pages";
+
 
 export function LoggedNavigation() {
   return(
-    <div>
-      <Button secondary>LoggedNavigation</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/artists' element={<Artists />} />
+        <Route path='/artist/:id' element={<Artist />} />
+        <Route path='/albums' element={<Albums />} />
+        <Route path='/album/:id' element={<Album />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
