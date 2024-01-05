479ca4801e0e1b748eb21c19b8037691478a4b1b
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-05 16:47:34 -0300

    Fit options for register and login

--- src/components/Auth/AuthOptions/AuthOptions.js
+++ src/components/Auth/AuthOptions/AuthOptions.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Button } from "semantic-ui-react";
+import "./AuthOptions.scss";
 
 export function AuthOptions(props) {
   const { openLogin, openRegister } = props;
 
   return (
-    <div style={{backgroundColor: "#000"}}>
-      <h1>AuthOptions</h1>
+    <div className="auth-options">
+      <h1>Disconnect and just listen music</h1>
 
-      <Button primary onClick={openRegister}>
+      <Button className="register" onClick={openRegister}>
         Register
         </Button>
-      <Button secondary onClick={openLogin}>
+      <Button className="login" onClick={openLogin}>
         Login
       </Button>
     </div>
