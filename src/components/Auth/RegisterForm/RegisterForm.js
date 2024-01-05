fd5c08c18b198860cf125edb62971032bdb4bd10
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-05 18:18:48 -0300

    Fit register form

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Form, Icon } from 'semantic-ui-react';
 
 export function RegisterForm(props) {
   const { openRegister, goBack } = props; 
 
   return (
-    <div>
-    <h1>RegisterForm</h1>
-    <Button primary onClick={openRegister}>Register</Button>
-    <Button secondary onClick={goBack}>Back</Button>
+    <div className="register-form">
+      <h1>Make your register and enjoy</h1>
+      
+        <Form>
+
+          <Form.Input
+            type="text"
+            placeholder="email"
+            icon="mail ouline"
+          />
+
+          <Form.Input
+            type="password"
+            placeholder="password"
+            icon={
+              <Icon
+                name="eye"
+                link
+                onClick={() => console.log("Show Password")}
+              />
+            }
+          />
+
+          <Form.Input
+            type="text"
+            placeholder="How you like to be called?"
+            icon="user circle outline"  
+          />
+
+          <Form.Button type="submit" primary fluid>
+            Send
+          </Form.Button>
+
+        </Form>
     </div>
   );
 }
