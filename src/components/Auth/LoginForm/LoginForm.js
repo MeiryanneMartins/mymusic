3b505efc3ef38ea0c4fc639d65d8a85ac2a2ed59
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-09 10:45:23 -0300

    Fit login form

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import { Button } from "semantic-ui-react";
+import React, {useState} from "react";
+import { Form, Icon } from "semantic-ui-react";
+import "./LoginForm.scss";
 
 export function LoginForm(props) {
-  const { openLogin, goBack } = props;
+  const { goBack } = props;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onShowHidenPassword = () => setShowPassword((prevState) => !prevState);
 
   return (
-    <div style={{ backgroundColor: "#f0f" }}>
-      <h1>LoginForm</h1>
+    <div className="login-form">
+      <h1>Free music</h1>
+    <Form>
+      <Form.Input
+      name="mail"
+      type="text"
+      placeholder="email"
+      icon="mail outline"
+      />
 
-      <Button primary onClick={openLogin}>Login</Button>
-      <Button secondary onClick={goBack}>Back</Button>
+      <Form.Input
+      name="password"
+      type={showPassword ? "text" : "password"}
+      placeholder="password"
+      icon={
+        <Icon
+        name={showPassword ? "eye slash" : "eye"}
+        link
+        onClick={onShowHidenPassword}
+        />
+      }
+      />
+
+      <Form.Button type="submit" primary fluid>
+        SEND
+      </Form.Button>
+
+    </Form>
+    <div className="login-form__options">
+      <p onClick={goBack}>back</p>
+    </div>
     </div>
   );
 }
