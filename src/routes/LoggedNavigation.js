bd213568835ca031515372697b59cc927b663461
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-09 14:56:22 -0300

    Create structure for layout on home

--- src/routes/LoggedNavigation.js
+++ src/routes/LoggedNavigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoggedLayout } from '../layouts/LoggedLayout/LoggedLayout';
 import { Home, Artists, Artist, Albums, Album, Profile } from "../pages";
 
 
 export function LoggedNavigation() {
   return(
     <BrowserRouter>
-      <Routes>
+    <LoggedLayout>
+    <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/artists' element={<Artists />} />
         <Route path='/artist/:id' element={<Artist />} />
@@ -14,6 +16,7 @@ export function LoggedNavigation() {
         <Route path='/album/:id' element={<Album />} />
         <Route path='/profile' element={<Profile />} />
       </Routes>
+    </LoggedLayout>
     </BrowserRouter>
   )
 }
