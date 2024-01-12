0594699aa8358ffb53385e275bcb5460c1490aa1
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-12 12:43:27 -0300

    layout: settings structure

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Button } from "semantic-ui-react";
+import { User } from "../../api";
+import "./Profile.scss";
+
+const userController = new User();
 
 export function Profile() {
+  const { displayName, email } = userController.getMe();
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <h1>Settings</h1>
+
+      <div className="profile__block">
+        <div>
+          <p>AVATAR</p>
+          <span>{ displayName }</span>
+        </div>
+        <Button onClick={() => console.log("update")}>
+          update
+        </Button>
+      </div>
+
+      <div className="profile__block">
+        <span>Email: {email}</span>
+        <Button onClick={() => console.log("update email")}>
+          update
+        </Button>
+      </div>
+
+
+      <div className="profile__block">
+        <span>Password: *** *** *** </span>
+        <Button onClick={() => console.log("update password")}>
+          update
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
