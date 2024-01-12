0b70d6b357cf72b8fd197561e632a3cb8f08abc2
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-12 19:02:23 -0300

    feat: control file for avatar

--- src/components/Profile/AvatarUpdate/AvatarUpdate.js
+++ src/components/Profile/AvatarUpdate/AvatarUpdate.js
@@ -1,19 +1,26 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import { Image } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
+import { User } from "../../../api";
 import { defaultUser } from "../../../assets";
 import "./AvatarUpdate.scss";
 
+const userController = new User();
+
 export function AvatarUpdate() {
+  const { photoURL } = userController.getMe();
+  const [avatarUrl, setAvatarUrl] = useState(photoURL || defaultUser);
+
   const onDrop = useCallback(async (acceptedFile) => {
-    console.log(acceptedFile);
+    const file = acceptedFile[0];
+    setAvatarUrl(URL.createObjectURL(file))
   });
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
   return (
-    <div className="avatar-update" {...getRootProps}>
+    <div className="avatar-update" {...getRootProps()}>
       <input {...getInputProps()} />
-      <Image src={defaultUser} />
+      <Image src={avatarUrl} />
     </div>
   );
 }
