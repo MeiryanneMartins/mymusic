f2249f14dfcd933ac55a4e9bf28d87b629b3cbec
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-14 10:28:07 -0300

    fix: add showModal property on component BasicaModal

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "semantic-ui-react";
 import { AvatarUpdate } from "../../components/Profile";
 import { BasicModal } from "../../components/Shared";
@@ -8,40 +8,73 @@ import "./Profile.scss";
 const userController = new User();
 
 export function Profile() {
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+  const [contentModal, setContentModal] = useState(null);
+
   const { displayName, email } = userController.getMe();
+
+  const onCloseModal = () => {
+    setShowModal(false);
+    setTitleModal("");
+    setContentModal(null);
+  };
+
+  const openForm = (type) => {
+    if (type === "displayName") {
+      setTitleModal("update name and nickname");
+      setContentModal(<h1>Form DisplayName</h1>);
+    }
+
+    if (type === "email") {
+      setTitleModal("update email");
+      setContentModal(<h1>Form email</h1>);
+    }
+
+    if (type === "password") {
+      setTitleModal("update password");
+      setContentModal(<h1>Form Password</h1>);
+    }
+
+    setShowModal(true);
+  };
 
   return (
     <>
-    <div className="profile">
-      <h1>Settings</h1>
+      <div className="profile">
+        <h1>Settings</h1>
 
-      <div className="profile__block">
-        <div>
-          <AvatarUpdate />
-          <span>{ displayName }</span>
+        <div className="profile__block">
+          <div>
+            <AvatarUpdate />
+            <span>{displayName}</span>
+          </div>
+          <Button onClick={() => openForm("displayName")}>
+            update
+          </Button>
         </div>
-        <Button onClick={() => console.log("update")}>
-          update
-        </Button>
+
+        <div className="profile__block">
+          <span>Email: {email}</span>
+          <Button onClick={() => openForm("email")}>
+            update
+          </Button>
+        </div>
+
+        <div className="profile__block">
+          <span>Password: *** *** *** </span>
+          <Button onClick={() => console.log("update password")}>
+            update
+          </Button>
+        </div>
       </div>
 
-      <div className="profile__block">
-        <span>Email: {email}</span>
-        <Button onClick={() => console.log("update email")}>
-          update
-        </Button>
-      </div>
-
-
-      <div className="profile__block">
-        <span>Password: *** *** *** </span>
-        <Button onClick={() => console.log("update password")}>
-          update
-        </Button>
-      </div>
-    </div>
-
-    <BasicModal show={true} onClose={() => console.log("close modal")} title="update data" children={<h1>content modal</h1>} /> 
+      <BasicModal
+        show={showModal}
+        onClose={onCloseModal}
+        title={titleModal}
+        children={contentModal}
+      />
     </>
   );
 }
