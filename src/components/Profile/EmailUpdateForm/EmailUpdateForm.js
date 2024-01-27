833936056b8fb2ff8e820b1094f557b49c972340
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-01-27 08:30:59 -0300

    feat: Control and validation emailUpdateForm

--- src/components/Profile/EmailUpdateForm/EmailUpdateForm.js
+++ src/components/Profile/EmailUpdateForm/EmailUpdateForm.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { Form } from "semantic-ui-react";
+import { useFormik } from 'formik';
+import { initialValues, validationSchema } from "./EmailUpdateForm.data";
 
 export function EmailUpdateForm() {
   const [showPassword, setShowPassword] = useState(false);
 
   const onShowHidenPassword = () => setShowPassword((prevState) => !prevState);
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.log(formValue)
+    },
+  });
+
   return (
-    <Form>
-      <Form.Input name="email" placeholder="new email" />
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="email"
+        placeholder="new email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+      />
+
       <Form.Input
         name="password"
         type={showPassword ? "text" : "password"}
@@ -18,9 +36,12 @@ export function EmailUpdateForm() {
           link: true,
           onClick: onShowHidenPassword,
         }}
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.errors.password}
       />
 
-      <Form.Button type="submit" fluid primary>
+      <Form.Button type="submit" fluid primary loading={formik.isSubmitting}>
         update email
       </Form.Button>
     </Form>
