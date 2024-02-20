0e4018b31180214c29cb47360267efac29041958
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-02-20 18:53:56 -0300

    feat: create modal for open forms to create artists, albuns and songs

--- src/components/Layout/LeftMenu/LeftMenu.js
+++ src/components/Layout/LeftMenu/LeftMenu.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Menu } from "semantic-ui-react";
 import { Link, useLocation } from "react-router-dom";
+import { BasicModal } from "../../Shared";
 import "./LeftMenu.scss";
 
 
 export function LeftMenu() {
   const { pathname } = useLocation();
 
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+  const [contentModal, setContentModal] = useState(null);
+
   const isCurrentpage = (route) => {
     return route === pathname
   }
 
+  const closeModal = () => {
+    setShowModal(false);
+    setTitleModal("");
+    setContentModal(null);
+  };
+
+  const openModal = (type) => {
+    if (type === "artist") {
+      setContentModal("New artist");
+      setContentModal(<p>Form new artist</p>)
+    }
+
+    if (type === "album") {
+      setTitleModal("New album");
+      setContentModal(<p>Form new album</p>)
+    }
+
+    if (type === "song") {
+      setTitleModal("New song");
+      setContentModal(<p>Form new song</p>)
+    }
+
+    setShowModal(true);
+  };
+
   return (
+    <>
     <div className="left-menu">
       <Menu secondary vertical fluid>
         <Menu.Item
@@ -44,22 +75,30 @@ export function LeftMenu() {
           name=" add new song"
           icon="plus"
           link
-          onClick={() => console.log("upload music")}
+          onClick={() => openModal("song")}
         />
 
         <Menu.Item
           name="add new album?"
           icon="plus"
           link
-          onClick={() => console.log("upload album")}
+          onClick={() => openModal("album")}
         />
         <Menu.Item
-          name="add new music?"
+          name="add new artist"
           icon="plus"
           link
-          onClick={() => console.log("upload music")}
+          onClick={() => openModal("artist")}
         />
       </Menu>
     </div>
+
+    <BasicModal
+      show={showModal}
+      onClose={closeModal}
+      title={titleModal}
+      childen={contentModal}
+    />
+    </>
   );
 }
