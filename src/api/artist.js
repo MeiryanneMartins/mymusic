698a4409f8146fc292856d2206d1933fd7421556
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-02-27 09:10:12 -0300

    feat: Add on api artist create

---
+++ src/api/artist.js
@@ -0,0 +1,21 @@
+import { setDoc, doc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+import { db } from "../utils";
+
+export class Artist {
+  collectionName = "artists";
+
+  async create(name) {
+    try {
+      const idArtist = uuidv4();
+      const created_at = new Date();
+      const data = { id: idArtist, name, created_at };
+      const docRef = doc(db, this.collectionName, idArtist);
+
+      await setDoc(docRef, data);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
