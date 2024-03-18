0cb2475d49c600b6f8d6623e75a97b31db26437e
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-03-18 20:06:24 -0300

    feat: add validation on album form

---
+++ src/components/Album/NewAlbumForm/NewAlbumForm.data.js
@@ -0,0 +1,15 @@
+import * as Yup from "yup";
+
+export function initialValues() {
+  return {
+    name: "",
+    artist:"",
+  };
+}
+
+export function validationSchema() {
+  return Yup.object({
+    name: Yup.string().required(true),
+    artist: Yup.string.required(true),
+  });
+}
