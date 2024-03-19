bb37c6d5304a50b1f356c62857ce89e0b41ff9f4
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-03-19 10:16:21 -0300

    feat:  get all artist

--- src/api/artist.js
+++ src/api/artist.js
@@ -1,5 +1,6 @@
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc, doc, collection, getDocs } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
+import { map } from "lodash";
 import { db } from "../utils";
 
 export class Artist {
@@ -13,9 +14,18 @@ export class Artist {
 
       const docRef = doc(db, this.collectionName, idArtist);
       await setDoc(docRef, data);
-      
     } catch (error) {
       throw error;
+    }
+  }
+
+  async obtainAll() {
+    try {
+      const docRef = collection(db, this.collectionName);
+      const snapshot = await getDocs(docRef);
+      return map(snapshot.docs, (doc) => doc.data());
+    } catch (error) {
+      
     }
   }
 }
