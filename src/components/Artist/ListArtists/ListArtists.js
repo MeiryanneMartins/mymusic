e4175e26a3d54e418653af48e34ea14301d2a8ad
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-03-20 21:25:05 -0300

    feat: add artist list component

---
+++ src/components/Artist/ListArtists/ListArtists.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Grid, Loader } from "semantic-ui-react";
+import { map, size } from "lodash";
+import { Link } from 'react-router-dom';
+import "./ListArtists.scss";
+
+export function ListArtists(props) {
+  const { artists } = props;
+
+  if(size(artists)===0){
+    return (
+      <Loader active incline="centered" size="large">
+        Loading...
+      </Loader>
+    );
+  }
+  return (
+    <Grid className="list-artists">
+      <Grid.Row columns={5}>
+        {map(artists, (artist) => {
+          <Grid.Column key={artist.id} className="list-artists__artist">
+            <p>{artist.name}</p>
+          </Grid.Column>
+        })}
+      </Grid.Row>
+    </Grid>
+
+  )
+}
