41f7838cccc0d08bce91def94b8d056150a39d19
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-04-11 20:14:36 -0300

    feat: create component new song form

--- src/components/Song/NewSongForm/NewSongForm.js
+++ src/components/Song/NewSongForm/NewSongForm.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Icon } from "semantic-ui-react";
+import classNames from "classnames";
 import "./NewSongForm.scss";
 
-export function NewSongForm(){
+export function NewSongForm() {
+  const [songName, setSongName] = useState("")
+
   return (
-    <div>
-      <h2>NewSongFOrm</h2>
-    </div>
-  )
+    <Form classNames="add-song-form">
+      <Form.Input name="name" placeholder="Name song" />
+      <Form.Dropdown
+        placeholder="album"
+        fluid
+        search
+        selection
+        options={[]}
+      />
+
+      <div className={classNames("add-song-form_file", {
+        error: false,
+      })}
+      >
+        <Icon name="cloud upload" />
+
+        <div>
+          <p>
+            take you song here
+          </p>
+          {songName && <p className="song-name">{songName}</p>}
+        </div>
+      </div>
+      <Form.Button type="submit" primary fluid>
+        send song
+      </Form.Button>
+    </Form>
+  );
 }
