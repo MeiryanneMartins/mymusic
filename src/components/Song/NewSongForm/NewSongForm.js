f37a57f10dbf7765bcb636bf898c692d9af86c8f
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-04-15 19:23:04 -0300

    feat: add on new song form validation

--- src/components/Song/NewSongForm/NewSongForm.js
+++ src/components/Song/NewSongForm/NewSongForm.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import { Form, Icon } from "semantic-ui-react";
 import classNames from "classnames";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from './NewSongForm.data';
 import "./NewSongForm.scss";
 
 export function NewSongForm() {
-  const [songName, setSongName] = useState("")
+  const [songName] = useState("");
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      console.console.log(formValue);
+    },
+  });
 
   return (
-    <Form classNames="add-song-form">
-      <Form.Input name="name" placeholder="Name song" />
+    <Form classNames="add-song-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="name"
+        placeholder="Name song"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        error={formik.errors.name}
+      />
+
       <Form.Dropdown
         placeholder="album"
         fluid
@@ -30,7 +48,7 @@ export function NewSongForm() {
           {songName && <p className="song-name">{songName}</p>}
         </div>
       </div>
-      <Form.Button type="submit" primary fluid>
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         send song
       </Form.Button>
     </Form>
